2794164524d41700def8274e9505ea0baba8ce40
marriammu <mariammuhammed1@gmail.com>
2022-09-24 01:23:31 +0200

    Edited Task 4

--- Tasks/Task 4/Core/Inc/EXTI.h
+++ Tasks/Task 4/Core/Inc/EXTI.h
@@ -7,7 +7,8 @@
 #define EXTI_FTSR     (DEFINE_REG(0x40013C00,0x0c))
 #define EXTI_PR     (DEFINE_REG(0x40013C00,0x14))
 
-void Interrupt_Mask_REG(char PinNum);
+void Interrupt_Mask_Enable(char PinNum);
+void Interrupt_Mask_Disable(char PinNum);
 void Falling_Edge(char PinNum);
 
 void Pending_REG(char PinNum);
--- Tasks/Task 4/Core/Src/EXTI.c
+++ Tasks/Task 4/Core/Src/EXTI.c
@@ -1,8 +1,12 @@
 #include "EXTI.h"
 
-void Interrupt_Mask_REG(char PinNum){
-	*(EXTI_IMR) |=(0x01<<PinNum);
-}
+
+ void Interrupt_Mask_Enable(char PinNum){
+	 *(EXTI_IMR) |=(0x01<<PinNum);
+ }
+ void Interrupt_Mask_Disable(char PinNum){
+	 *(EXTI_IMR) |= (0x00<<PinNum);
+ }
 void Falling_Edge(char PinNum)
 {
 	*(EXTI_FTSR) |=(0x01<<PinNum);
